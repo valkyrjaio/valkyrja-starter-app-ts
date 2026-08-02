0c1b16a51c3f2fb64e75385408cf517e5f1273a7
Melech Mizrachi <melechmizrachi@gmail.com>
2026-08-01 19:07:27 -0700

    [CopyrightHeader] ci: Cover the .github/ci files with the header and formatter.

--- .github/ci/eslint/eslint.config.js
+++ .github/ci/eslint/eslint.config.js
@@ -1,3 +1,12 @@
+/*
+ * This file is part of the Valkyrja Application package.
+ *
+ * (c) Melech Mizrachi <melechmizrachi@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import path from 'path';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
@@ -26,11 +35,13 @@ const copyrightHeader = {
             Program(node) {
                 const sourceCode = context.sourceCode;
                 const comments = sourceCode.getAllComments();
-                const first = comments
-                    .filter((c) => c.type === 'Block')
-                    .sort((a, b) => a.range[0] - b.range[0])[0];
+                const first = comments.filter((c) => c.type === 'Block').sort((a, b) => a.range[0] - b.range[0])[0];
 
-                if (first === undefined || first.loc.start.line !== 1 || !first.value.includes('This file is part of the Valkyrja Application package.')) {
+                if (
+                    first === undefined ||
+                    first.loc.start.line !== 1 ||
+                    !first.value.includes('This file is part of the Valkyrja Application package.')
+                ) {
                     context.report({
                         node,
                         messageId: 'missing',
@@ -44,26 +55,22 @@ const copyrightHeader = {
     },
 };
 
-export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.strictTypeChecked,
-    {
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: path.resolve(import.meta.dirname, '../../..'),
-            },
-        },
-        plugins: {
-            local: { rules: { 'copyright-header': copyrightHeader } },
-        },
-        rules: {
-            'local/copyright-header'                           : 'error',
-            '@typescript-eslint/no-namespace'                  : 'off',
-            '@typescript-eslint/no-extraneous-class'           : 'off',
-            '@typescript-eslint/no-unnecessary-type-parameters': 'off',
-            '@typescript-eslint/no-unused-vars'                : ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-            '@typescript-eslint/no-invalid-void-type'          : ['error', { allowAsThisParameter: true }],
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.strictTypeChecked, {
+    languageOptions: {
+        parserOptions: {
+            projectService: true,
+            tsconfigRootDir: path.resolve(import.meta.dirname, '../../..'),
         },
     },
-);
+    plugins: {
+        local: { rules: { 'copyright-header': copyrightHeader } },
+    },
+    rules: {
+        'local/copyright-header': 'error',
+        '@typescript-eslint/no-namespace': 'off',
+        '@typescript-eslint/no-extraneous-class': 'off',
+        '@typescript-eslint/no-unnecessary-type-parameters': 'off',
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-invalid-void-type': ['error', { allowAsThisParameter: true }],
+    },
+});
